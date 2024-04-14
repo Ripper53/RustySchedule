46a2d6089f7a71f60ce29c1acf12484462dcdea6
Albar <GalacticHyperStar@gmail.com>
2024-04-14 12:45:52 -0400

    Uncomment disabling Windows terminal.

--- rusty-schedule-app/src/main.rs
+++ rusty-schedule-app/src/main.rs
@@ -1,4 +1,4 @@
-//#![windows_subsystem = "windows"]
+#![windows_subsystem = "windows"]
 use std::{io::{self, Write}, sync::mpsc::{channel, Receiver, Sender}, thread::JoinHandle, time::Duration};
 
 use args::{ScheduleCli, ScheduleCommand};
